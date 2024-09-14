6f54347ae9f902302b195629f910b9ca9896b6d0
Naeem Baghi <baghinaeem@gmail.com>
2024-09-14 14:31:36 +0330

    feat: default sort to -date

--- utils/getLedgerCommand.ts
+++ utils/getLedgerCommand.ts
@@ -3,7 +3,7 @@ import getEnv from './getEnv';
 const getLedgerCommand = () => {
   const LedgerFile = getEnv().LEDGER_FILE;
   const PriceDBFile = getEnv().LEDGER_PRICE_DB;
-  return `ledger --file ${LedgerFile} --price-db ${PriceDBFile}`;
+  return `ledger --file ${LedgerFile} --price-db ${PriceDBFile} --sort -date`;
 };
 
 export default getLedgerCommand;
